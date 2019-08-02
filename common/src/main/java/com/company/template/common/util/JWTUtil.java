89df929e2312d4dad9a2fe34d2c1d3a5a1f9c4b9
woohaifeng <woohaifeng@hotmail.com>
2019-08-02 17:03:18 +0800

    .gitignore

---
+++ common/src/main/java/com/company/template/common/util/JWTUtil.java
@@ -0,0 +1,13 @@
+package com.company.template.common.util;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class JWTUtil {
+    private SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS512;
+
+    public static String generateToken() {
+        return "";
+    }
+
+
+}
